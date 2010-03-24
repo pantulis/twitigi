b0a5826e3d8b3c7fa4c978f663e9c5358de18f52
Juan Lupión <juan.lupion@the-cocktail.com>
2010-03-24 23:33:36 +0100

    Initial commit

---
+++ twitigi.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+from os.path import dirname, join as join_path
+sys.path.insert(0, join_path(dirname(__file__), 'lib')) # extend sys.path
+
+from wsgiref.handlers import CGIHandler
+import wsgiref.handlers
+import yaml
+
+
+from google.appengine.ext.webapp import RequestHandler, WSGIApplication
+from google.appengine.ext import webapp, db
+from google.appengine.api import users
+from google.appengine.ext.webapp import template
+
+from oauth import OAuthHandler, OAuthClient
+
+
+class MyIndexHandler(webapp.RequestHandler):
+	def get(self):
+		client = OAuthClient('twitter', self)
+                
+                if not client.get_cookie():
+                        view = 'not_logged.html'
+                        template_values = {}
+                else:
+                        template_values = {
+                                'username': client.get('/account/verify_credentials')['screen_name'],
+                                'twits': client.get('/statuses/friends_timeline', 'GET', (200,), count = 100)
+                                }
+                        view = 'index.html'
+
+                path = os.path.join(os.path.dirname(__file__) + '/templates/' + view )
+                self.response.out.write(template.render(path, template_values))
+
+        def twitter_feeds(self, client):
+            the_twits = client.get('/statuses/friends_timeline', 'GET', (200,), count=100)
+            res = "<ul>"
+            url_regex = re.compile(r'''((?:mailto:|ftp://|http://)[^ <>'"{}|\\^`[\]]*)''')
+            for twit in the_twits:
+                twit_username = twit['user']['screen_name']
+                twit_content = twit['text']
+
+                if (re.search("http", twit_content)):
+                    res += "<li>"
+                    res += "<strong>" + twit_username + "</strong> : " + url_regex.sub(r'<a href="\1">\1</a>', twit_content)
+                    res += "</li>"
+
+            res += "</ul>"
+            return res
+
+
+def main():
+	application = webapp.WSGIApplication([
+                ('/oauth/(.*)/(.*)', OAuthHandler),
+                ('/', MyIndexHandler)
+        ], debug=True)
+        
+	wsgiref.handlers.CGIHandler().run(application)	
+
+if __name__ == '__main__': main()
